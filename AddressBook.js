411da54f902d4e78274b8bf727342588896038a0
RagulKanna <ragulragi619@gmail.com>
2021-10-15 22:44:37 +0530

    Added:Validated Details of contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,3 +1,11 @@
+const name_Regex = "^([A-Z]{1}[a-z]{2,})$";
+const address_Regex = "^([a-zA-Z0-9 ,]{4,})$";
+const city_Regex = "^([a-zA-Z0-9 ,]{4,})$";
+const state_Regex = "^([a-zA-Z0-9 ,]{4,})$";
+const zip_Regex = "^([0-9]{3}\\s{0,1}[0-9]{3})$";
+const phone_Number_Regex = "^([0-9]{2}\\s{1}[0-9]{10})$";
+const email_Regex = "^([a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$)";
+
 class Contact {
     first_Name;
     last_Name;
@@ -18,12 +26,17 @@ class Contact {
         this.phone_Number = params[6];
         this.email = params[7];
     }
+
     get first_Name() {
         return this._first_Name;
     }
 
     get last_Name() {
         return this._last_Name;
+    }
+
+    get address() {
+        return this.address;
     }
 
     get city() {
@@ -46,32 +59,101 @@ class Contact {
         return this.email;
     }
 
+
+    /**
+     * @param {string} first_Name
+     */
     set first_Name(first_Name) {
-        this._first_Name = first_Name;
+        let result = new RegExp(name_Regex, 'g').test(first_Name);
+        if (result) {
+            this._firstName = first_Name;
+        }
+        else
+            throw "Incorrect Name Format";
     }
 
+    /**
+     * @param {string} last_Name
+     */
     set last_Name(last_Name) {
-        this._last_Name = last_Name;
+        let result = new RegExp(name_Regex, 'g').test(first_Name);
+        if (result) {
+            this._last_Name = last_Name;
+        }
+        else
+            throw "Incorrect Name Format";
+
+    }
+    /**
+     * @param {string} address
+     */
+    set address(address) {
+        let result = new RegExp(address_Regex, 'g').test(first_Name);
+        if (result) {
+            this._address = address;
+        }
+        else
+            throw "Incorrect Address Name Format";
     }
 
+    /**
+     * @param {string} city
+     */
     set city(city) {
-        this._city = city;
+        let result = new RegExp(city_Regex, 'g').test(first_Name);
+        if (result) {
+            this._city = city;
+        }
+        else
+            throw "Incorrect City Name Format";
     }
 
+    /**
+     * @param {string} state
+     */
     set state(state) {
-        this._state = state;
+        let result = new RegExp(state_Regex, 'g').test(first_Name);
+        if (result) {
+            this._state = state;
+        }
+        else
+            throw "Incorrect State Name Format";
     }
 
+    /**
+     * @param {string} zip
+     */
     set zip(zip) {
-        this._zip = zip;
+        let result = new RegExp(zip_Regex, 'g').test(first_Name);
+        if (result) {
+            this._zip = zip;
+        }
+        else
+            throw "Incorrect Zip Format";
     }
 
+    /**
+     * @param {string} phone_Number
+     */
     set phone_Number(phone_Number) {
-        this._phone_Number = phone_Number;
+        let result = new RegExp(phone_Number_Regex, 'g').test(first_Name);
+        if (result) {
+            this._phone_Number = phone_Number;
+        }
+        else
+            throw "Incorrect Phone Number Format";
     }
 
+    /**
+     * @param {string} email
+     */
     set email(email) {
-        this._email = email;
+        let result = new RegExp(email_Regex, 'g').test(first_Name);
+        if (result) {
+            this._email = email;
+        }
+        else
+            throw "Incorrect Email Format";
     }
 
     toString() {
@@ -79,6 +161,10 @@ class Contact {
     }
 }
 
-let new_contact = new Contact("Ragul", "Kanna", "Sankari", "Salem", "TamilNadu",637301,9500744651,"ragulragi619@gmail.com");
-console.log("Welcome to address book program....")
-console.log(new_contact.toString());
+console.log("Welcome to address book program....");
+try {
+    let new_contact = new Contact("ragul", "kanna", "Sankari", "Salem", "TamilNadu", "637 301", "91 9500744651", "ragulragi619@gmail.com");
+    console.log(new_contact.toString());
+} catch (e) {
+    console.log(e);
+}
